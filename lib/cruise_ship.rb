f7a7caaf5fb31f861d1c237c0ef3748bd1bb43e6
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-15 22:15:01 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,8 +9,7 @@
 def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner = "#{name}"
-      winner
+      "#{name}"
     end
   end
 end
